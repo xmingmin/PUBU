3eff9333f24900a18047b598adb3ed250a5826c6
mingmin <mingmin@gmail.com>
2016-08-22 11:28:26 +0800

    这是个测试

---
+++ Pu/Pu/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Pu
+//
+//  Created by xiemingmin on 16/8/22.
+//  Copyright © 2016年 xiemingmin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
